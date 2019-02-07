29b791a9e800018513b7e437030f55780b27ff5b
Aida Razavilar <aida.razavilar@gmail.com>
2019-02-07 10:09:05 -0800

    Unit 3 additions

---
+++ Unit03/src/DistanceRunner.java
@@ -0,0 +1,37 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class DistanceRunner
+{
+	public static void main( String[] args )
+	{
+		//add test cases
+		Scanner keyboard = new Scanner(in);
+		out.print("Enter the X1 :: ");
+		int x1 = keyboard.nextInt();
+
+		out.print("Enter the X2 :: ");
+		int x2 = keyboard.nextInt();
+
+		out.print("Enter the Y1 :: ");
+		int y1 = keyboard.nextInt();
+
+		out.print("Enter the Y2 :: ");
+		int y2 = keyboard.nextInt();
+		
+		
+		Distance test = new Distance(x1, y1, x2, y2);
+		test.calcDistance();
+		test.print();
+			
+	}
+}
