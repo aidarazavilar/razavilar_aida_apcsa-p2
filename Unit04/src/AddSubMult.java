31662f46b12f77dbb3a2e5c5ddc195f78d20ba86
Aida Razavilar <aida.razavilar@gmail.com>
2019-02-11 10:02:23 -0800

    Unit 4 finished

---
+++ Unit04/src/AddSubMult.java
@@ -0,0 +1,28 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*; 
+import java.util.Scanner;
+
+public class AddSubMult
+{
+	public static double check( double a, double b )
+	{
+		double result = 0;
+		if (a==b) {
+			result= a*b;
+		}
+		
+		if (a>b) {
+			result= a - b;
+		}
+		
+		if (a<b) {
+			result= b - a;
+			
+		}
+		
+		return (double)Math.round(result*10)/10;
+	}
+}
---
+++ dumbexample/src/dumbexample/castingexampleclass.java
@@ -0,0 +1,13 @@
+package dumbexample;
+
+public class castingexampleclass {
+	public static void main(String[] args) {
+//		int x = 4, y=5;
+//		 double v=x+y;
+//		 double w=v;
+		
+		float f = 5.0f + 6.0f;
+		
+		 System.out.println();
+	}
+}
